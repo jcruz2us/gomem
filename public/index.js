3f699b4aaaf2aa45cacb6ab5377978d4c6a07227
Jorge Cruz <fulbytes@gmail.com>
2016-04-11 00:45:15 -0700

    Display realtime memory breakdown pie chart

--- public/index.js
+++ public/index.js
@@ -1,7 +1,8 @@
 var es = new EventSource('/profile');
 
 var state = {
-    memstats: {}
+    memstats: {},
+    stream: []
 };
 
 var bytesToString = function(bytes) {
@@ -14,6 +15,50 @@ var bytesToString = function(bytes) {
         return fmt(bytes / 1024 / 1024) + 'MB';
     } else {
         return fmt(bytes / 1024 / 1024 / 1024) + 'GB';
+    }
+};
+
+var TotalSystemBreakdown = {
+    view: function() {
+        var total = bytesToString(state.stream[0] && state.stream[0].Sys);
+        var tsb = m('.total-system-breakdown', {
+            config: function(element, isInit, ctx) {
+                var mem = state.stream[0];
+                if (!isInit) {
+                    ctx.chart = c3.generate({
+                        bindto: element,
+                        data: {
+                            type: 'pie',
+                            columns: [
+                                ['HeapSys', 0],
+                                ['StackSys', 0],
+                                ['MSpanSys', 0],
+                                ['MCacheSys', 0],
+                                ['BuckHashSys', 0],
+                                ['GCSys', 0],
+                                ['OtherSys', 0]
+                            ]
+                        }
+                    });
+                } else {
+                    ctx.chart.flow({
+                        columns: [
+                            ['HeapSys', mem.HeapSys],
+                            ['StackSys', mem.StackSys],
+                            ['MSpanSys', mem.MSpanSys],
+                            ['MCacheSys', mem.MCacheSys],
+                            ['BuckHashSys', mem.BuckHashSys],
+                            ['GCSys', mem.GCSys],
+                            ['OtherSys', mem.OtherSys]
+                        ]
+                    });
+                }
+            }
+        });
+        return m('div', [
+            m('div', total),
+            tsb
+        ]);
     }
 };
 
@@ -30,12 +75,24 @@ m.mount(document.getElementById("stats"), {
                     'StackInuse',
                     'StackSys',
                     'Sys',
-                    'TotalAlloc'
+                    'TotalAlloc',
+                    'HeapReleased'
+                    //'HeapObjec'
                 ];
                 var retVal = {};
                 items.forEach(function(item) {
                     retVal[item] = bytesToString(state.memstats[item]);
                 });
+
+                var otherItems = [
+                    'HeapObjects',
+                    'NumGC',
+                    'PauseTotalNs'
+                ];
+                otherItems.forEach(function(item) {
+                    retVal[item] = state.memstats[item];
+                });
+
                 return retVal;
             }
         };
@@ -43,7 +100,7 @@ m.mount(document.getElementById("stats"), {
     },
     view: function(ctrl) {
         var stats = ctrl.stats();
-        return m('div', Object.keys(stats).map(function(key) {
+        var info = m('div', Object.keys(stats).map(function(key) {
             return m('div', {
                 key: key
             }, [
@@ -51,6 +108,11 @@ m.mount(document.getElementById("stats"), {
                 m('span', stats[key])
             ]);
         }));
+
+        return m('div', [
+            m(TotalSystemBreakdown),
+            info
+        ]);
     }
 });
 
@@ -98,6 +160,7 @@ var heapChart = c3.generate({
 
 es.addEventListener('message', function(e) {
     var mem = JSON.parse(e.data);
+    mem.timestamp = new Date().getTime();
     chart.flow({
         columns: [
             ['x', new Date().getTime()],
@@ -116,6 +179,7 @@ es.addEventListener('message', function(e) {
     });
     m.startComputation();
     state.memstats = mem;
+    state.stream.unshift(mem);
     m.endComputation();
     //console.log('message', JSON.parse(e.data));
 });
